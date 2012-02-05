04d28e6c370dd3da7de73767bdac4d076eb4b0e9
Abhishek Chavan <abhishek@wizni.com>
2012-02-05 21:04:07 +0530

    Changes

---
+++ Musictest/src/main/java/com/bezudar/examples/bitshifting/BitShifting.java
@@ -0,0 +1,10 @@
+package com.bezudar.examples.bitshifting;
+
+public class BitShifting {
+
+	public static void main(String[] args) {
+
+		System.out.println(Integer.toBinaryString(3));
+		System.out.println(-3 >>> 1);
+	}
+}
